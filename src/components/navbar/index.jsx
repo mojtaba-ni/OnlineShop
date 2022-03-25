742bbd874b320bd240654d0f0248970ff3639419
mojtaba-ni <mojtabanik@yahoo.com>
2022-03-26 02:05:18 +0430

    update navbar cmponent 1.1

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -3,24 +3,32 @@ import shop from '../../assets/navbar/shop.png'
 import searchIcon from '../../assets/navbar/searchicon.png'
 import shopIcon from '../../assets/navbar/iconshop.png'
 import '../../style/components/navbar.scss'
+import Navside from '../navside'
 
 export default () => {
   return (
     <div className='navbar'>
-      <div className="navbar-container">
-            <div className="user-container">
-                <div className='userShop-border'><img src={shop} alt="خرید" /></div>
-                <button>حساب کاربری</button>
+      <div div className="navbar-container">
+        <div className="user-container">
+          <div className='userShop-border'>
+            <div className="img-box">
+              <img src={shop} alt="خرید" />
             </div>
-            <div className="search-box">
-                <input type="text" placeholder='جستجو' />
-                <img src={searchIcon} alt="search" />
+            <div className="button-box">
+              <button>حساب کاربری</button>
             </div>
-            <div className="site-icon">
-                <h3>آنلاین شاپ</h3>
-                <img src={shopIcon} alt="آنلاین شاپ" />
-            </div>
+          </div>
         </div>
+        <div className="search-box">
+          <input type="text" placeholder='جستجو' />
+          <img src={searchIcon} alt="search" />
+        </div>
+        <div className="site-icon">
+          <h3>آنلاین شاپ</h3>
+          <img src={shopIcon} alt="آنلاین شاپ" />
+        </div>
+      </div>
+      <Navside/>
     </div>
   )
 }
