a7a421e4b505a073569f1ffc66e6a68e3c59e9d4
mojtaba-ni <mojtabanik@yahoo.com>
2022-03-28 22:13:19 +0430

    create contact component

---
+++ src/components/contact/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../../style/components/contact.scss'
+
+export default ({src}) => {
+  return (
+    <div className="contact">
+        <img src={src} alt="contact" />
+    </div>
+  )
+}
+
+ 
