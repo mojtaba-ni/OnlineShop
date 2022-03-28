0f349a5ff24c499406a5e991cbaca356dd7e8741
mojtaba-ni <mojtabanik@yahoo.com>
2022-03-28 15:05:30 +0430

    create Button component

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../../style/components/Button.scss'
+
+export default ({title}) => {
+  return (
+    <button className="button-contaner">
+        <p>{title}</p>
+    </button>
+  )
+}
+
+ 
