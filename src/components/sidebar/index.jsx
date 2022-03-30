8fe3c30001a6191d7d062b9ba07d42590e234864
mojtaba-ni <mojtabanik@yahoo.com>
2022-03-30 19:15:22 +0430

    create sidebar component

---
+++ src/components/sidebar/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import '../../style/components/sidebar.scss'
+
+export default () => {
+  return (
+    <div className='sidebar'>
+        <div className="sidebar-user">
+            <div className="button-box">
+              <button>حساب کاربری</button>
+            </div>
+        </div>
+        <nav>
+            <a href="">s</a>
+            <a href="">ss</a>
+            <a href="">sss</a>
+            <a href="">ssss</a>
+        </nav>
+    </div>
+  )
+}
+
+ 
