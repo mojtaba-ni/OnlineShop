3b189477bffa7a57fc5e6bb5e6b7923a7dc286a8
mojtaba-ni <mojtabanik@yahoo.com>
2022-03-30 19:13:54 +0430

    create MobileSearch component

---
+++ src/components/MobileSearch/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../../style/components/MobileSearch.scss'
+
+export default () => {
+  return (
+    <div className='search-container'>
+        <input type="text" placeholder='جستجو ...'/>
+    </div>
+  )
+}
+
+ 
