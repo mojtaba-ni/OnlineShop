5bf2a5367deb3cce2ae1e4be7e154f0f0528b61c
mojtaba-ni <mojtabanik@yahoo.com>
2022-04-19 03:22:19 +0430

    create the Datajs

---
+++ src/utils/gamePr.js
@@ -0,0 +1,159 @@
+import ps4 from '../assets/cardPr/ps4.png'
+import ps5 from '../assets/cardPr/ps5.png'
+import xbox1 from '../assets/cardPr/xbox1.png'
+import xbox from '../assets/cardPr/xbox.png'
+import monitor1 from '../assets/cardPr/monitor1.png'
+import monitor2 from '../assets/cardPr/monitor2.png'
+import monitor3 from '../assets/cardPr/monitor3.png'
+import monitor4 from '../assets/cardPr/monitor4.png'
+import group1 from '../assets/product/Group1.png'
+import group2 from '../assets/product/Group2.png'
+import group3 from '../assets/product/Group3.png'
+
+export const Pics = [
+    {
+        id:1,
+        title : 'Xbox Series X',
+        titleP:'کنسول بازی مایکروسافت',
+        img: xbox1, 
+        price:1290000,
+    },
+    {
+        id:2,
+        title : 'PlayStation 4',
+        titleP:'کنسول بازی مایکروسافت',
+        img:ps4,
+        price:150000,
+    },
+    {
+        id:3,
+        title : 'PlayStation 5',
+        titleP:'کنسول بازی مایکروسافت',
+        img:ps5,
+        price:2490000,
+    },
+    {
+        id:4,
+        title : 'Xbox',
+        titleP:'کنسول بازی مایکروسافت',
+        img:xbox,
+        price:990000,
+    },
+    {
+        id:5,
+        title : 'PlayStation 4',
+        titleP:'کنسول بازی مایکروسافت',
+        img:ps4,
+        price:1390000,
+    }
+]
+export const Monitor = [
+    {
+        id:1,
+        title : 'PHILIPS ',
+        titleP:'34 inch',
+        img: monitor1, 
+        price:1290000,
+    },
+    {
+        id:2,
+        title : 'LG UltraFine',
+        titleP:'34 inch',
+        img:monitor2,
+        price:150000,
+    },
+    {
+        id:3,
+        title : 'Asus ProArt',
+        titleP:'34 inch',
+        img:monitor3,
+        price:2490000,
+    },
+    {
+        id:4,
+        title : 'ASUS ROG ',
+        titleP:'34 inch',
+        img:monitor4,
+        price:990000,
+    },
+    {
+        id:5,
+        title : 'LG UltraFine',
+        titleP:'34 inch',
+        img:monitor1,
+        price:1390000,
+    }
+]
+export const Mobiles = [
+    {
+        id:1,
+        title : 'PHILIPS ',
+        titleP:'34 inch',
+        img:group1,
+        price:1290000,
+        change:true
+    },
+    {
+        id:2,
+        title : 'LG UltraFine',
+        titleP:'34 inch',
+        img:group2,
+        price:150000,
+        change:true
+    },
+    {
+        id:3,
+        title : 'Asus ProArt',
+        titleP:'34 inch',
+        img:group3,
+        price:2490000,
+        change:true
+    },
+    {
+        id:4,
+        title : 'ASUS ROG ',
+        titleP:'34 inch',
+        img:group2,
+        price:990000,
+        change:true
+    },
+    {
+        id:5,
+        title : 'LG UltraFine',
+        titleP:'34 inch',
+        img:group1,
+        price:1390000,
+        change:true
+    },
+    {
+        id:6,
+        title : 'LG UltraFine',
+        titleP:'34 inch',
+        img:group2,
+        price:1390000,
+        change:true
+    }
+]
+export const spechials = [
+    {
+        id:1,
+        title : 'Galaxy Z',
+        titleP:'گوشی موبایل سامسونگ',
+        img:group1,
+        price:21400000,
+    },
+    {
+        id:2,
+        title : 'Galaxy S21 FE',
+        titleP:'گوشی موبایل سامسونگ',
+        img:group2,
+        price:16000000,
+    },
+    {
+        id:3,
+        title : 'Redmi Note 11',
+        titleP:'گوشی موبایل شیائومی',
+        img:group3,
+        price:5490000,
+    },
+]
